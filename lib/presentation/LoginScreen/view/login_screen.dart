256bd4cf206ebaef103d1df85731967724ad34ea
nasi321 <nasinasri6102@gmail.com>
2024-11-13 15:58:39 +0530

    utils added

---
+++ lib/presentation/LoginScreen/view/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/color_constants.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage("assets/images/loginbg.png"),
+    fit: BoxFit.cover,
+    ),),
+      child: Column(
+        children: [
+        ],
+      ),)
+    );
+  }
+}
