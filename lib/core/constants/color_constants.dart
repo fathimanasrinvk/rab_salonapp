3839536f79379975a9f96078e730bca2b8f027fd
nasi321 <nasinasri6102@gmail.com>
2024-11-14 10:37:21 +0530

    splash added

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -5,7 +5,7 @@ class ColorTheme {
   static Color lightgrey = const Color(0xFFA3A3A3);
   static Color secondarycolor = const Color(0xffE4D3D7);
 
-  static Color white = const Color.fromARGB(255, 29, 20, 20);
+  static Color white = Colors.white;
   static Color black = const Color.fromARGB(255, 0, 0, 0);
   static Color red = const Color.fromARGB(255, 209, 0, 0);
   static Color green = const Color(0xFF00c254);
--- lib/presentation/LoginScreen/view/login_screen.dart
+++ lib/presentation/LoginScreen/view/login_screen.dart
@@ -6,14 +6,8 @@ import '../../../core/constants/color_constants.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/loginbg.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-    ));
+    );
   }
 }
