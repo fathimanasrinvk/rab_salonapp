2c6b79c78650b4999046368adf939409aef8002a
fathimathsafa <safahaneefa01@gmail.com>
2024-11-14 16:00:08 +0530

    bottom navigation added

--- lib/presentation/bottom_navigation_screen/view/bottom_navigation_screen.dart
+++ lib/presentation/bottom_navigation_screen/view/bottom_navigation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rab_salon/core/constants/color_constants.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -19,35 +20,53 @@ class _StatusNavigationBarState extends State<StatusNavigationBar> {
   // List of screens corresponding to the tab index
   final List<Widget> _screens = [
     Container(
-      color: Colors.red,
-      child: Center(child: Text("Work Board Screen")),
-    ),
+        color: Colors.white, child: Center(child: Text("Work Board Screen"))),
     Container(
-      color: Colors.green,
-      child: Center(child: Text("Attendances Screen")),
-    ),
+        color: Colors.white, child: Center(child: Text("Attendances Screen"))),
     Container(
-      color: Colors.blue,
-      child: Center(child: Text("Projects Screen")),
-    ),
+        color: Colors.white, child: Center(child: Text("Projects Screen"))),
+    Container(
+        color: Colors.white, child: Center(child: Text("Reports Screen"))),
+    Container(
+        color: Colors.white, child: Center(child: Text("Settings Screen"))),
   ];
 
-  // List of icons and text for each bottom navigation item
+  // List of images, text, and color for each bottom navigation item
   final List<Map<String, dynamic>> _navItems = [
     {
-      "icon": Image.asset(""), 
+      "icon": "assets/images/serviceinactivebottom.png",
       "text": "Work Board",
-      "activeIcon": Icons.work_outline, // Active icon when selected
+      "activeIcon": "assets/images/serviceactivebottom.png",
+      "activeColor": ColorTheme.black,
+      "inactiveColor": ColorTheme.white,
     },
     {
-      "icon": Icons.calendar_today, 
-      "text": "Attendances",
-      "activeIcon": Icons.calendar_today_outlined,
+      "icon": "assets/images/employeesinactivebottom.png",
+      "text": "Employees",
+      "activeIcon": "assets/images/employeesactivebottom.png",
+      "activeColor": ColorTheme.black,
+      "inactiveColor": ColorTheme.white,
     },
     {
-      "icon": Icons.note, 
-      "text": "Projects",
-      "activeIcon": Icons.note_add_outlined,
+      "icon": "assets/images/ownerinactivebottom.png",
+      "text": "Owner",
+      "activeIcon": "assets/images/owneractivebottom.png",
+      "activeColor": ColorTheme.black,
+      "inactiveColor": ColorTheme.white,
+    },
+    {
+      "icon": "assets/images/companyinactivebottom.png",
+      "text": "Company",
+      "activeIcon": "assets/images/companyactivebottom.png",
+      "activeColor": ColorTheme.black,
+      "inactiveColor": ColorTheme.white,
+    },
+    {
+      "icon": "assets/images/settingsinactivebottom.png",
+      "text": "Settings",
+      "activeIcon": "assets/images/settingsactivebottom.png",
+      "activeColor": ColorTheme.black,
+      "inactiveColor": ColorTheme.white,
     },
   ];
 
@@ -60,21 +79,33 @@ class _StatusNavigationBarState extends State<StatusNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
+
     return Scaffold(
       body: _screens[_selectedIndex], // Show the selected screen
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: ColorTheme.maincolor, // Change background color
         items: List.generate(_navItems.length, (index) {
           final item = _navItems[index];
           return BottomNavigationBarItem(
-            icon: Icon(
-              _selectedIndex == index ? item["activeIcon"] : item["icon"], 
-              size: 30,
+            icon: Image.asset(
+              _selectedIndex == index ? item["activeIcon"] : item["icon"],
+              width: size.width * .06,
+              height: size.height * .03,
             ),
             label: item["text"],
+            // Change the text color based on selection
+            backgroundColor: Colors.white,
           );
         }),
+        selectedLabelStyle:
+            TextStyle(color: _navItems[_selectedIndex]['activeColor']),
+        unselectedLabelStyle: TextStyle(color: Colors.grey),
+        selectedItemColor: _navItems[_selectedIndex]['activeColor'],
+        unselectedItemColor: ColorTheme.white,
       ),
     );
   }
